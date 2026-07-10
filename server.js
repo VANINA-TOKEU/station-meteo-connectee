7b4aac78c3ea3567c2ca143034d5ebdbffadf915
VANINA-TOKEU <vaninatokeu@gmail.com>
2026-07-10 05:56:20 +0200

    correction variables MySQL Railway

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const db = mysql.createPool({
   port: process.env.MYSQLPORT || 3306,
   waitForConnections: true,
   connectionLimit: 10
-});;
+});
 
 // ===============================
 // DONNÉES TEMPS RÉEL
